52b4d63a569ddf5a68f924cf9cf9c438642be78e
Breno-MT <brenomtdev@gmail.com>
2025-07-21 22:18:16 -0300

    FEAT: added REST_FRAMEWORK configuration and quickstart app to the INSTALLED_APPS

--- drfapp/settings.py
+++ drfapp/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # my apps
-    'rest_framework'
+    'rest_framework',
+    'quickstart'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
